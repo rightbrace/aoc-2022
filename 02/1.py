6fb1388678dba6410a49c114ee7dda8bab4c380c
Josef Duchesne <josef.duchesne@purolator.com>
2022-12-02 10:25:09 -0500

    day 2

---
+++ 02/1.py
@@ -0,0 +1,23 @@
+def chunk_input(filename="input.txt"):
+    with open(filename) as file:
+        return [chunk.strip() for chunk in file.read().strip().split("\n\n")]
+
+move_name_map = {"A": "R", "X": "R", "B": "P", "Y": "P", "C": "S", "Z": "S"} # Convert to R,P,S
+loses_to = {"R": "S", "P": "R", "S": "P"} # Given a move, what does it beat?
+shape_scores = {"R": 1, "P": 2, "S": 3} # How much is each move worth?
+
+# Get score from game result
+def result(a, b):
+    if a == b:
+        return 3
+    if b == loses_to[a]:
+        return 6
+    return 0
+
+# Extract moves from line, get total score
+def score_line(line):
+    elf = move_name_map[line[0]]
+    you = move_name_map[line[2]]
+    return shape_scores[you] + result(you, elf)
+
+print(sum(map(score_line, open("input.txt").readlines())))
